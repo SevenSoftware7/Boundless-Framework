f9720adc0ce17b1b0960dd1718272234fb382441
AxelSeven <spaghettiandmeatballs61@gmail.com>
2024-05-24 05:17:56 +0200

    Fixed operation ordering error in Stacking Attribute Modifiers

--- Project/Scripts/Core/Item/Entity/Attribute/MultiAttributeModifier.cs
+++ Project/Scripts/Core/Item/Entity/Attribute/MultiAttributeModifier.cs
@@ -5,15 +5,15 @@ using System.Linq;
 
 
 public struct MultiAttributeModifier(IEnumerable<IAttributeModifier>? attributes = null) : IAttributeModifier {
-	public IEnumerable<IAttributeModifier> Attributes { get; private set; } = FlattenAttributes(attributes ?? []);
+	public IEnumerable<IAttributeModifier> Attributes { get; private set; } = FlattenAttributes(attributes?.OfType<IAttributeModifier>() ?? []);
 	public readonly bool IsStacking => true;
 
 	private static IEnumerable<IAttributeModifier> FlattenAttributes(IEnumerable<IAttributeModifier> attributes) {
-		foreach (IAttributeModifier discount in attributes) {
-			if (discount is MultiAttributeModifier multi) {
+		foreach (IAttributeModifier attribute in attributes) {
+			if (attribute is MultiAttributeModifier multi) {
 				foreach (IAttributeModifier contained in multi.Attributes) yield return contained;
 			}
-			yield return discount;
+			yield return attribute;
 		}
 	}
 
@@ -21,18 +21,14 @@ public struct MultiAttributeModifier(IEnumerable<IAttributeModifier>? attributes
 	public readonly float ApplyTo(float baseValue) {
 		float result = baseValue;
 
-		IEnumerable<IAttributeModifier> stackingAttributes = Attributes.Where(a => a.IsStacking);
-		foreach (IAttributeModifier attribute in stackingAttributes) {
-			if (attribute is null) continue;
-
-			result += attribute.ApplyTo(baseValue) - baseValue;
+		IEnumerable<IAttributeModifier> nonStackingAttributes = Attributes.Where(a => ! a.IsStacking);
+		foreach (IAttributeModifier attribute in nonStackingAttributes) {
+			result = attribute.ApplyTo(result);
 		}
 
-		IEnumerable<IAttributeModifier> nonStackingAttributes = Attributes.Except(stackingAttributes);
-		foreach (IAttributeModifier attribute in nonStackingAttributes) {
-			if (attribute is null) continue;
-
-			result = attribute.ApplyTo(result);
+		IEnumerable<IAttributeModifier> stackingAttributes = Attributes.Except(nonStackingAttributes);
+		foreach (IAttributeModifier attribute in stackingAttributes) {
+			result += attribute.ApplyTo(baseValue) - baseValue;
 		}
 
 		return result;
