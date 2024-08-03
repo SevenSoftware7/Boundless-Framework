3692d0c9dc88125cbf494e2410a62a4839a2819f
AxelSeven <spaghettiandmeatballs61@gmail.com>
2024-08-03 04:43:46 +0200

    Compartmentalized Physics Layers

--- Project/Scripts/Core/CollisionLayers.cs
+++ Project/Scripts/Core/CollisionLayers.cs
@@ -2,8 +2,10 @@ namespace LandlessSkies.Core;
 
 public static class CollisionLayers {
 	public static readonly uint Terrain = 1 << 0;
-	public static readonly uint Entity = 1 << 1;
-	public static readonly uint Water = 1 << 2;
-	public static readonly uint Interactable = 1 << 3;
-	public static readonly uint Damage = 1 << 4;
+	public static readonly uint Water = 1 << 1;
+
+	public static readonly uint Entity = 1 << 8;
+	public static readonly uint Prop = 1 << 9;
+	public static readonly uint Interactable = 1 << 10;
+	public static readonly uint Damage = 1 << 11;
 }
