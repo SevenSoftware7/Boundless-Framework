389da169fb9ba0f98082b247e00cbf4821044133
AxelSeven <spaghettiandmeatballs61@gmail.com>
2024-10-14 02:16:56 +0200

    fix(namespace): Moved ItemRegistry to Core

--- Scripts/Core/ItemRegistry.cs
+++ Scripts/Core/ItemRegistry.cs
@@ -1,10 +1,9 @@
+namespace LandlessSkies.Core;
+
 using System.Collections.Generic;
 using System.Linq;
 using Godot;
 using Godot.Collections;
-using LandlessSkies.Core;
-
-namespace LandlessSkies.Vanilla;
 
 [Tool]
 [GlobalClass]
