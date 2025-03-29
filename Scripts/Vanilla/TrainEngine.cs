28d3189ef0ca63ad702427c2aff0dcfd5a2cce6d
AxelSeven <spaghettiandmeatballs61@gmail.com>
2025-03-29 14:25:06 +0100

    fix: Change speed type from float to double for improved precision in TrainEngine

--- Scripts/Vanilla/TrainEngine.cs
+++ Scripts/Vanilla/TrainEngine.cs
@@ -3,9 +3,9 @@ namespace LandlessSkies.Vanilla;
 using Godot;
 
 public partial class TrainEngine : PathFollow3D {
-	[Export] public float speed = 3f;
+	[Export] public double speed = 3f;
 
 	public override void _Process(double delta) {
-		Progress += speed * (float)delta;
+		Progress += (float)(speed * delta);
 	}
 }
